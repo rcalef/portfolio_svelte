13363e050749b7c8041aebf56f1ba770433a3b6b
Robert Calef <robert.calef@gmail.com>
2024-04-09 15:33:41 -0400

    Maybe fix rendering page contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,10 @@
 	<title>Robert Calef's Homepage</title>
 </svelte:head>
 
-
-<body>
-    <h1>Robert Calef</h1>
-    <p>Exciting times making webpages. This is me and my grandma:</p>
-    <img src="images/grandma.jpg" alt="Me and my grandma" height="400">
-    <p
-        >And this is a long string of text to test how the current design is handling
-        very long texts: \Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas blanditiis minus nisi libero atque provident eligendi, consectetur officia vel impedit fugit harum molestiae, voluptate, nobis cum deserunt temporibus animi quibusdam?
-    </p>
-</body>
+<h1>Robert Calef</h1>
+<p>Exciting times making webpages. This is me and my grandma:</p>
+<img src="images/grandma.jpg" alt="Me and my grandma" height="400">
+<p
+    >And this is a long string of text to test how the current design is handling
+    very long texts: \Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas blanditiis minus nisi libero atque provident eligendi, consectetur officia vel impedit fugit harum molestiae, voluptate, nobis cum deserunt temporibus animi quibusdam?
+</p>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,8 +2,8 @@
 	<title>Contact me</title>
 </svelte:head>
 
-<body>
-    <h1>Contact Info</h1>
+<h1>Contact Info</h1>
+<p>
     My email is <code>&lt;first_initial&gt;&lt;last_name&gt;@mit.edu</code>. You can also find me on <a href="https://www.linkedin.com/in/robert-calef/">LinkedIn</a>.
     For a sense of my background, here's my <a href="./docs/rcalef_CV_2024.pdf">CV</a>.
-</body>
+</p>
